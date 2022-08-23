452f9cb086fdaecaabe7e185fbdcbf54333b2ccd
MariaT20 <mariatendo84@gmail.com>
2022-08-23 04:52:34 -0700

    C

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,8 +8,8 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *current = list -> next;
-	listint_t *post_current = current -> next;
+	listint_t *current = list->next;
+	listint_t *post_current = current->next;
 
 	if (current != NULL && post_current != NULL)
 		return (1);
