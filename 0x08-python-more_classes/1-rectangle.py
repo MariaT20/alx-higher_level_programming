0682042cf9b096d19351eaa04a164f70f53706b5
MariaT20 <mariatendo84@gmail.com>
2022-09-19 08:43:52 -0700

    ptyhon more classe

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""docsting for Rectangle"""
+
+
+class Rectangle:
+    """doctring for init"""
+    def __init__(self, width, height):
+        self.height = height
+        self.width = width
+    """width getter method"""
+    @property
+    def width(self):
+        return self.__width
+    """height getter method"""
+    @property
+    def height(self):
+        return self.__height
+    """width getter method"""
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int) and value >= 0:
+            self.__width = value
+        elif not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+    """height getter method"""
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int) and value >= 0:
+            self.__height = value
+        elif not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
