c40cc33ed4e008dc054ebca839bf914b714e1967
MariaT20 <mariatendo84@gmail.com>
2022-09-20 12:51:39 -0700

    Test driven development in pthyon

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-'''
-add_integer module
-'''
+"""Defines an integer addition function."""
 
 
-def add_integer(a, b):
+def add_integer(a, b=98):
+    """Return the integer addition of a and b.
+    Float arguments are typecasted to ints before addition is performed.
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-    Return sum of a and b.
-    """
-    if type(a) == float:
-        a = int(a)
-    if type(b) == float:
-        b = int(b)
-
-    if not isinstance(a, int) or isinstance(a, bool):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) or isinstance(b, bool):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-
-    return ( a + b )
+    return (int(a) + int(b))
