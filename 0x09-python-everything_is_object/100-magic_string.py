f500c547f3222c35bd82db1c10459212a3b1b22d
MariaT20 <mariatendo84@gmail.com>
2022-09-21 06:07:14 -0700

    txt file

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.counter = getattr(magic_string, "counter", 0) + 1
-    return ", ".join(["Holberton" for i in range(magic_string.counter)])
+    magic_string.c, t = (magic_string.__dict__.get('c', 0) + 1, ', Holberton')
+    return '{:s}{:s}'.format(t[2:], t * (magic_string.c - 1))
