64deafa71f0fcfc30f787153cd06df2d3d63c469
MariaT20 <mariatendo84@gmail.com>
2022-09-21 05:48:00 -0700

    Test driven development in python

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
-'''
-module: say_my_name
-'''
+"""A module that contains a function that prints the full name of a person"""
 
 
-def say_my_name(first_name, last_name=''):
-    ''' print first and last name, make the big bucks
-    Keyword arguments:
-    first_name -- string
-    last_name -- string
-    '''
-
-    #  ERROR MESSAGE DICT  #
-    err_msg = {}
-    err_msg["FirstNotStr"] = "first_name must be a string"
-    err_msg["LastNotStr"] = "last_name must be a string"
-
-    #  TESTS  #
-    if type(first_name) != str:
-        raise TypeError(err_msg["FirstNotStr"])
-    if type(last_name) != str:
-        raise TypeError(err_msg["LastNotStr"])
-
-    #  OUTPUT  #
-    print("My name is {}".format(first_name), end='')
-    if len(last_name) == 0:
-        print()
-    else:
-        print(" {}".format(last_name))
+def say_my_name(first_name, last_name=""):
+    """Says the full name of a person
+    Args:
+        first_name (str): the first name provided
+        last_name (str): the last name provided - optional
+    Format of output:
+        My name is <first_name> <last_name>
+    Exceptions:
+        TypeError - If first_name or last_name is not a string
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
