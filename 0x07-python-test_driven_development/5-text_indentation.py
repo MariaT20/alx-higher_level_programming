d48781bea30d5f9c57d43dadc9dc81f5b7f99594
MariaT20 <mariatendo84@gmail.com>
2022-09-21 05:44:39 -0700

    Test driven development in python

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""Module Documentation"""
+"""Contains a function that indent text passed as argument"""
 
 
 def text_indentation(text):
-    """ Indents text based on preceding characters """
-    chars = ['.', '?', ':']
+    """prints a text with 2 new lines after each of '.', '?' and ':'
+    Args:
+        text (str): the passed string
+    """
     if not isinstance(text, str):
-        raise TypeError('text must be a string')
-    else:
-        for c in chars:
-            if c in text:
-                text_split = text.split(c)
-                text = str()
-                for i in range(len(text_split) - 1):
-                    text += f'{text_split[i].strip()}\n\n'
-                text += f'{text_split[i + 1].strip()}'
-        print(text)
+        raise TypeError("text must be a string")
+
+    i, j = 0, 0
+    for a, c in enumerate(text):
+        if c in ('.', ':', '?'):
+            print(text[i:j+1], end="\n\n")
+            i = j + 2
+        else:
+            if j == len(text) - 1:
+                print(text[i:], end="")
+        j += 1
