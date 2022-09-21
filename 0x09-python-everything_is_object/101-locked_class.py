bd160a414cab4319fadb80b67e2e283146db6b81
MariaT20 <mariatendo84@gmail.com>
2022-09-21 06:08:18 -0700

    corrections on txt file

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""A module that implements a LockedClass"""
+
+
 class LockedClass:
-    __slots__ = "first_name"
+    """defines a class that prevents the user from dynamically creating
+    attributes, except if it is first_name"""
+    __slots__ = ["first_name"]
