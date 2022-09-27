5a3e243c06559de1e96d3a29191b1bed9cb3eef3
MariaT20 <mariatendo84@gmail.com>
2022-09-27 09:18:56 -0700

    python input and output files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-'''
+"""
 A module that reads a text file
-'''
+"""
 
 
-def read_file(filename=''):
-    '''
+def read_file(filename=""):
+    """
      reads the the text file (UTF8) and prints it to stdout
      Returns none
-     '''
-    with open(filename, 'r', encoding = 'utf-8') as fn:
-        print(fn.read(), end='')
+     """
+    with open(filename, "r", encoding="utf-8") as fn:
+        print(fn.read(), end="")
